f0a5ff998f96592def5eb95e6bca12e5eafa33ed
ehannum <ehannum@gmail.com>
2015-12-24 01:41:02 -0800

    added client-side display of existing events

--- src/calendar.js
+++ src/calendar.js
@@ -1,5 +1,5 @@
 $(function () {
-  calendar = $('#calendar').clndr({
+  var calendar = $('#calendar').clndr({
     template: $('#clndr-temp').html(),
     daysOfTheWeek: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
   });
@@ -12,15 +12,35 @@ $(function () {
     return false;
   });
 
-  var colors = ['blue', 'purple', 'orange', 'green', 'pink', 'yellow'];
-  // var thisMonth = moment.months((new Date()).getMonth());
+  var colors = ['blue', 'purple', 'pink', 'orange', 'yellow', 'green'];
+  var thisMonth = moment.months((new Date()).getMonth());
 
   $.get('/events', function (data) {
     console.log(data);
-    // for (var i = 0; i < data.length; i++) {
-    //   console.log(data[i]);
-    // }
+    for (var i = 0; i < data.length; i++) {
+      createTrip(data[i].startDate.iso, data[i].endDate.iso, data[i].name, data[i].comment);
+    }
   });
+
+  var createTrip = function (start, end, name, comment) {
+    var tripDates = [];
+
+    date = (new Date(start)).setUTCHours(8);
+    end = (new Date(end)).setUTCHours(8);
+
+    while (date <= end) {
+      tripDates.push({date: date, color: colors[0]});
+
+      date = (new Date(date + 86400000)).getTime();
+    }
+
+    tripDates[0].name = name + ' arrives';
+    tripDates[tripDates.length - 1].name = name + ' leaves';
+
+    colors.push(colors.shift());
+
+    calendar.addEvents(tripDates);
+  };
 
   $('.new-trip').submit(function (evt) {
     evt.preventDefault();
